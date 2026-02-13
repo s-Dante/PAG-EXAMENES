181680ba3b8b23b2f73e72c473321ec78c1906e6
s-Dante <146044384+s-Dante@users.noreply.github.com>
2026-02-13 14:41:42 -0600

    sql

--- config.php
+++ config.php
@@ -10,22 +10,22 @@
 // ],
 // ];
 
-// return [
-//     "database" => [
-//         "host"     => getenv('MYSQLHOST') ?: "localhost",
-//         "user"     => getenv('MYSQLUSER') ?: "root",
-//         "password" => getenv('MYSQLPASSWORD') ?: "",
-//         "dbname"   => getenv('MYSQLDATABASE') ?: "pagExamenes",
-//         "port"     => getenv('MYSQLPORT') ?: "3306",
-//     ],
-// ];
-
 return [
     "database" => [
-        "host"     => getenv('PGHOST'),
-        "user"     => getenv('PGUSER'),
-        "password" => getenv('PGPASSWORD'),
-        "dbname"   => getenv('PGDATABASE'),
-        "port"     => getenv('PGPORT'),
+        "host"     => getenv('MYSQLHOST') ?: "localhost",
+        "user"     => getenv('MYSQLUSER') ?: "root",
+        "password" => getenv('MYSQLPASSWORD') ?: "",
+        "dbname"   => getenv('MYSQLDATABASE') ?: "pagExamenes",
+        "port"     => getenv('MYSQLPORT') ?: "3306",
     ],
 ];
+
+// return [
+//     "database" => [
+//         "host"     => getenv('PGHOST'),
+//         "user"     => getenv('PGUSER'),
+//         "password" => getenv('PGPASSWORD'),
+//         "dbname"   => getenv('PGDATABASE'),
+//         "port"     => getenv('PGPORT'),
+//     ],
+// ];
