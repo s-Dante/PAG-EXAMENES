bf6370ab4d5408554cc08de226d2378a45c85fa1
s-Dante <146044384+s-Dante@users.noreply.github.com>
2026-02-13 13:59:52 -0600

    Cambios para host

--- Model/Conexion.php
+++ Model/Conexion.php
@@ -1,21 +1,21 @@
 <?php
 
-class Conexion{
+class Conexion
+{
     private $con;
     //crea la conexion en el constructor, pasandole de parametros, todos los datos
     // de la conexion que se dinieron en config.php
-    public function __construct($config){
+    public function __construct($config)
+    {
         //crea el dsn con los datos, y el ";" que es el separador que pondra por cada parametro
-        $dsn = 'mysql:' . http_build_query($config,"",";");
-
-        $this->con = new PDO($dsn,$config['user'],$config['password']);
+        //$dsn = 'mysql:' . http_build_query($config,"",";");
+        $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset=utf8mb4";
+        $this->con = new PDO($dsn, $config['user'], $config['password']);
         $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
-    public function getCon(){
+    public function getCon()
+    {
         return $this->con;
     }
-
 }
-
-?>
--- config.php
+++ config.php
@@ -1,11 +1,21 @@
 <?php
 
-return [
+// return [
 
-"database" => [
-  "host" => "localhost",
-  "user" => "root",
-  "password" => "",
-  "dbname" => "pagExamenes",
-],
+// "database" => [
+//   "host" => "localhost",
+//   "user" => "root",
+//   "password" => "",
+//   "dbname" => "pagExamenes",
+// ],
+// ];
+
+return [
+    "database" => [
+        "host"     => getenv('MYSQLHOST') ?: "localhost",
+        "user"     => getenv('MYSQLUSER') ?: "root",
+        "password" => getenv('MYSQLPASSWORD') ?: "",
+        "dbname"   => getenv('MYSQLDATABASE') ?: "pagExamenes",
+        "port"     => getenv('MYSQLPORT') ?: "3306",
+    ],
 ];
